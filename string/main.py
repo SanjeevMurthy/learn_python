18259c77b27e5e8e81a253a154c187560271de81
SanjeevMurthy <sanjeevmurthy01@gmail.com>
2022-04-20 17:40:44 +0530

    string examples

---
+++ string/main.py
@@ -0,0 +1,14 @@
+long_string = '''
+multi line
+string
+'''
+
+reboot = '1'
+
+reboot1 = bool('False')
+
+if reboot:
+    print("Do reboot", int(reboot))
+
+if reboot1:
+    print("Do reboot", reboot1)
